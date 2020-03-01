c648df3c3f7b278e2b539aebe62ea60530ca66d6
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2020-03-01 19:27:59 +0100

    failing for good reason

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,13 +1,6 @@
 public class Fibonacci {
+
     public static int[] generate(int length) {
-        int[] sequence = new int[length];
-        for (int i = 0; i < sequence.length; i++) {
-            if(i < 2){
-                sequence[i] = i;
-            } else {
-                sequence[i] = sequence[i-1] + sequence[i -2];
-            }
-        }
-        return sequence;
+        return new int[]{-1};
     }
 }
--- src/test/java/FibonacciShould.java
+++ src/test/java/FibonacciShould.java
@@ -1,10 +1,13 @@
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertThat;
 
 public class FibonacciShould {
+
     @Test
-    public void generateFirstEightNumbers() {
-        assertArrayEquals(new int[]{0,1,1,2,3,5,8,13}, Fibonacci.generate(8));
+    public void generate_zero_for_first_number() {
+        assertThat(Fibonacci.generate(1)[0], is(0));
     }
 }
