278092ecd0dd0d7a410ae07b314ef50e5a67d147
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2020-03-01 19:46:37 +0100

    passing

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -6,7 +6,7 @@ public class Fibonacci {
             if (i < 2) {
                 sequence[i] = i;
             } else {
-                sequence[i] = i - 1;
+                sequence[i] = sequence[i - 1] + sequence[i - 2];
             }
         }
         return sequence;
